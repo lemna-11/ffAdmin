517fc7fce7148925ee5033bad67802047f5f5c90
Mano <lemmensmano@gmail.com>
2023-06-03 19:46:51 +0200

    and i oop

--- src/main/java/com/example/ffadmin/HelloApplication.java
+++ src/main/java/com/example/ffadmin/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.BootstrapFX;
 
 import java.io.IOException;
 
@@ -12,8 +13,9 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Fast Food Admin App");
         stage.setScene(scene);
+        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
         stage.show();
     }
 
--- src/main/java/com/example/ffadmin/HelloController.java
+++ src/main/java/com/example/ffadmin/HelloController.java
@@ -4,11 +4,27 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public Label aRatherSussyLabel;
+
+    @FXML
+    public void openProductPage(){
+        aRatherSussyLabel.setText("A sussy product line, hmmmmmmmmmmmmm");
+    }
+
+    @FXML
+    public void openOrdersPage(){
+        aRatherSussyLabel.setText("A sussy order even, maybe some cocaine milord?");
+    }
+
+    @FXML
+    public void OpenCategoriesPage(){
+        aRatherSussyLabel.setText("WE ONLY SELL AMONG US MERCH AND DRUGS");
+    }
+
+    @FXML
+    public void exitApp(){
+        System.exit(0);
     }
 }
